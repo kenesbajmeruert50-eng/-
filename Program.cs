5517f3468c3f1dc0083f0acdfb8d5801b4798aca
kenesbajmeruert50-eng <kenesbajmeruert50@gmail.com>
2026-04-08 12:02:29 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,11 +2,18 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем поддержку контроллеров с представлениями
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Статические файлы (css, js)
+app.UseStaticFiles();
+
+// Маршрутизация
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
